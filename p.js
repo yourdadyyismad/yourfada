16e688b7c553534b3938ab37838608f4ba103ad8
yourdadyyismad <plsworm6@gmail.com>
2025-02-19 18:58:33 +0100

    Update p.js

--- p.js
+++ p.js
@@ -1,9 +1,15 @@
 const { chromium } = require('playwright')
 
 
+(async () => {
 const browser = await chromium.launch()
 const context = await browser.newContext()
 const page = await context.newPage ()
 
-await page.goto('https://github.com/BLUEXDEMONI')
+await page.goto('https://github.com/suhail')
 
+const [username] = await page.locator('.p-name vcard-fullname d-block overflow-hidden').allInnerTexts();
+
+console.log(username);
+await browser.close();
+  })();
